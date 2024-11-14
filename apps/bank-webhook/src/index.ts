26bb595ac0af2eb013a923d30132b5690b879b15
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-14 15:31:13 +0530

    balance & txn update logic done

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,12 +2,43 @@ import express from "express";
 const app = express();
 import db from "@repo/db/client";
 
-app.post("/hdfcWebhook", (req, res) => {
+app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add a zod validation here?
+    // check if this req actually came from hdfc bank, use a webhook secret
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    //Updtae balance in db, add txn
+    
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: paymentInformation.userId
+                },
+                data: {
+                    amount: {
+                        increment: paymentInformation.amount
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where: {
+                   token: paymentInformation.token, 
+                },
+                data: {
+                    status: "Success"
+                }
+            })
+        ]);
+        res.json({
+            message: "Captured"
+        })
+
+    } catch(e) {
+        res.status(411).json({
+            message: "Error while parsing webhook"
+        })
+    }
 })
